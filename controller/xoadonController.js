a625cd0ebfa71605b0a6c6583330cba8a07bc0ae
DuyPhan9696 <84225998+DuyPhan9696@users.noreply.github.com>
2022-02-17 12:03:42 +0700

    update-folder

---
+++ controller/xoadonController.js
@@ -0,0 +1,8 @@
+const { off } = require('../dbConnection/dbConnection');
+const connection = require('../dbConnection/dbConnection');
+
+module.exports.suadon = (req, res) => {
+    let madon = req.body.madon
+
+    let delsql =
+}
